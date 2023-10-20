c340c762a1be207e862cd0d8584acf054458e7f0
Marina <marinalaguna2004@gmail.com>
2023-10-20 14:01:39 +0200

    creada la funcion para el inicio de sesion

--- js/app.js
+++ js/app.js
@@ -5,3 +5,18 @@ let usuario = {
     correo: "marinalaguna2004@gmail.com",
     contrasena: "admin123"
 }
+
+function validarInicio(){
+    var correo = document.querySelector('input[type="email]').ariaValueMax
+    var contrasena = document.querySelector('input[type = "password"]').value
+    var mensaje = validarEmail()
+
+    if (mensaje === ''){
+        if (validarEmail.trim() == '' || contrasena.trim() == ''){
+            mensaje += 'El correo y la contraseña no pueden estar vacíos.'
+            return mensaje
+        }
+    }else{
+
+    }
+}
