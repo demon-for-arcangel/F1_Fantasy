e9d4f98bb71e2c89b061b3a049dda07ac51b809f
Marina <marinalaguna2004@gmail.com>
2023-10-27 14:21:21 +0200

    update inicio.js

--- js/inicio.js
+++ js/inicio.js
@@ -1,34 +1,34 @@
 //carga inicial
 // Supongamos que tenemos un arreglo de grandes premios con puntuaciones
 const grandesPremios = [
-    // ... (lista de grandes premios con puntuaciones)
+    //(lista de grandes premios con puntuaciones)
 ];
 
 // Supongamos que también tenemos una lista de usuarios bot con sus pilotos
 const usuariosBot = [
-// ... (lista de usuarios bot con sus pilotos)
+    // (lista de usuarios bot con sus pilotos)
 ];
 
 // Cargar grandes premios y usuarios bot
 function cargarInicial() {
-cargarGrandesPremios();
-generarUsuariosBot();
+    cargarGrandesPremios();
+    generarUsuariosBot();
 }
 
 function cargarGrandesPremios() {
-// Código para cargar los grandes premios
+    // Código para cargar los grandes premios
 }
 
 function generarUsuariosBot() {
-// Código para generar usuarios bot
+    // Código para generar usuarios bot
 }
 
 // Asignar piloto titular y suplente a tu usuario
 function asignarPilotos(usuario) {
-// Código para asignar pilotos titular y suplente a tu usuario
+    // Código para asignar pilotos titular y suplente a tu usuario
 }
 
 // Ordenar pilotos alfabéticamente
 function ordenarPilotosAlfabeticamente() {
-// Código para ordenar pilotos alfabéticamente
+    // Código para ordenar pilotos alfabéticamente
 }
