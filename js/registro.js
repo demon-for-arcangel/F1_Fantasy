198ebbaf86cda85ca2f28d280bfe6a837627ebe0
ismasarry <145430109+ismasarry@users.noreply.github.com>
2023-10-31 14:13:06 +0100

    quito las n

--- js/registro.js
+++ js/registro.js
@@ -1,16 +1,17 @@
 
 function validarRegistro() {
+
     var nombre = document.getElementById("nombre").value;
     var apellidos = document.getElementById("apellidos").value;
-    var contraseña = document.getElementById("contraseña").value;
-    var verificacionContraseña = document.getElementById("verificacionContraseña").value;
+    var contrasena = document.getElementById("contrasena").value;
+    var verificacionContrasena = document.getElementById("verificacionContrasena").value;
     var correo = document.getElementById("correo").value;
     var nick = document.getElementById("nick").value;
 
 
     var nombreRegExp = /^[A-Za-z]{2,20}$/;
     var apellidosRegExp = /^[A-Za-z]{2,30}$/;
-    var contraseñaRegExp = /^[\w*#$]{6,12}$/;
+    var contrasenaRegExp = /^[\w*#$]{6,12}$/;
     var correoRegExp = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
     var nickRegExp = /^[A-Za-z_]{4,10}$/;
 
@@ -24,12 +25,12 @@ function validarRegistro() {
         errores.push("El campo Apellidos tiene que tener entre 2 y 30 caracteres.");
     }
 
-    if (!contraseñaRegExp.test(contraseña)) {
-        errores.push("El campo Contraseña tiene que tener entre 6 y 12 caracteres y puede contener los siguientes caracteres especiales *#$.");
+    if (!contrasenaRegExp.test(contrasena)) {
+        errores.push("El campo Contrasena tiene que tener entre 6 y 12 caracteres y puede contener los siguientes caracteres especiales *#$.");
     }
 
-    if (contraseña !== verificacionContraseña) {
-        errores.push("La verificación de contraseña no coincide con la contraseña.");
+    if (contrasena !== verificacionContrasena) {
+        errores.push("La verificación de contrasena no coincide con la contrasena.");
     }
 
     if (!correoRegExp.test(correo)) {
@@ -48,24 +49,23 @@ function validarRegistro() {
 }
 
 
-function validarContraseña() {
-    var contraseña = document.getElementById("contraseña").value;
-    var mensajeError = document.getElementById("mensajeErrorContraseña");
+function validarContrasena() {
+    var contrasena = document.getElementById("contrasena").value;
+    var mensajeError = document.getElementById("mensajeErrorContrasena");
+ 
+    var contrasenaRegExp = /^[\w*#$]{6,12}$/;
 
-    
-    var contraseñaRegExp = /^[\w*#$]{6,12}$/;
-
-    if (contraseñaRegExp.test(contraseña)) {
+    if (contrasenaRegExp.test(contrasena)) {
         mensajeError.textContent = "";
     } else {
-        mensajeError.textContent = "La contraseña es inválida. Debe contener de 6 a 12 caracteres y puede incluir solo *, #, $ y caracteres alfanuméricos.";
+        mensajeError.textContent = "La contrasena es inválida. Debe contener de 6 a 12 caracteres y puede incluir solo *, #, $ y caracteres alfanuméricos.";
     }
 }
 function limpiarCampos() {
     document.getElementById("nombre").value = "";
     document.getElementById("apellidos").value = "";
-    document.getElementById("contraseña").value = "";
-    document.getElementById("verificacionContraseña").value = "";
+    document.getElementById("contrasena").value = "";
+    document.getElementById("verificacionContrasena").value = "";
     document.getElementById("correo").value = "";
     document.getElementById("nick").value = "";
 }
