caabb3a412ea235620ac34c0140a10a5a5acece4
ismasarry <145430109+ismasarry@users.noreply.github.com>
2023-11-05 11:00:36 +0100

    termino registro y enseguida subo el sass

--- js/registro.js
+++ js/registro.js
@@ -1,6 +1,4 @@
-
 function validarRegistro() {
-
     var nombre = document.getElementById("nombre").value;
     var apellidos = document.getElementById("apellidos").value;
     var contrasena = document.getElementById("contrasena").value;
@@ -8,83 +6,75 @@ function validarRegistro() {
     var correo = document.getElementById("correo").value;
     var nick = document.getElementById("nick").value;
 
-
     var nombreRegExp = /^[A-Za-z]{2,20}$/;
     var apellidosRegExp = /^[A-Za-z]{2,30}$/;
-    var contrasenaRegExp = /^[\w*#$]{6,12}$/;
+    var contrasenaRegExp = /^[A-Za-z0-9*#$]{6,12}$/;
     var correoRegExp = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
     var nickRegExp = /^[A-Za-z_]{4,10}$/;
 
     var errores = [];
 
+    var errorNombre = document.getElementById("errorNombre");
     if (!nombreRegExp.test(nombre)) {
-        errores.push("El campo Nombre tiene que tener entre 2 y 20 caracteres.");   
+        errores.push("El campo Nombre tiene que tener entre 2 y 20 caracteres.");
+        errorNombre.textContent = "El campo Nombre tiene que tener entre 2 y 20 caracteres.";
+    } else {
+        errorNombre.textContent = "";
     }
 
+    var errorApellidos = document.getElementById("errorApellidos");
     if (!apellidosRegExp.test(apellidos)) {
         errores.push("El campo Apellidos tiene que tener entre 2 y 30 caracteres.");
+        errorApellidos.textContent = "El campo Apellidos tiene que tener entre 2 y 30 caracteres.";
+    } else {
+        errorApellidos.textContent = "";
     }
 
+    var errorContrasena = document.getElementById("errorContrasena");
     if (!contrasenaRegExp.test(contrasena)) {
-        errores.push("El campo Contrasena tiene que tener entre 6 y 12 caracteres y puede contener los siguientes caracteres especiales *#$.");
+        errores.push("El campo Contraseña tiene que tener entre 6 y 12 caracteres y puede contener los siguientes caracteres especiales *#$.");
+        errorContrasena.textContent = "El campo Contraseña tiene que tener entre 6 y 12 caracteres y puede contener los siguientes caracteres especiales *#$.";
+    } else {
+        errorContrasena.textContent = "";
     }
 
+    var errorVerificacionContrasena = document.getElementById("errorVerificacionContrasena");
     if (contrasena !== verificacionContrasena) {
-        errores.push("La verificación de contrasena no coincide con la contrasena.");
+        errores.push("La verificación de contraseña no coincide con la contraseña.");
+        errorVerificacionContrasena.textContent = "La verificación de contraseña no coincide con la contraseña.";
+    } else {
+        errorVerificacionContrasena.textContent = "";
     }
 
+    var errorCorreo = document.getElementById("errorCorreo");
     if (!correoRegExp.test(correo)) {
-        errores.push("El campo Correo es inválido.");
+        errores.push("Asegúrate de que no haya caracteres especiales incorrectos y que el dominio tenga al menos dos letras en su extensión.");
+        errorCorreo.textContent = "Asegúrate de que no haya caracteres especiales incorrectos y que el dominio tenga al menos dos letras en su extensión.";
+    } else {
+        errorCorreo.textContent = "";
     }
 
+    var errorNick = document.getElementById("errorNick");
     if (!nickRegExp.test(nick)) {
-        errores.push("El campo Nick debe tener entre 4 y 10 caracteres y no puede contener caraceres especiales salvo (_ .)");
-    }
-
-    if (errores.length > 0) {
-        alert("Se encontraron los siguientes errores:\n" + errores.join("\n"));
+        errores.push("El campo Nick debe tener entre 4 y 10 caracteres y no puede contener caracteres especiales salvo (_ .)");
+        errorNick.textContent = "El campo Nick debe tener entre 4 y 10 caracteres y no puede contener caracteres especiales salvo (_ .)";
     } else {
-        alert("Registro exitoso. Redirigiendo a la página de inicio de sesión.");
-    }
-}
-
-
-function validarContrasena() {
-    var contrasena = document.getElementById("contrasena").value;
-    var mensajeError = document.getElementById("mensajeErrorContrasena");
- 
-    var contrasenaRegExp = /^[\w*#$]{6,12}$/;
-
-    if (contrasenaRegExp.test(contrasena)) {
-        mensajeError.textContent = "";
-    } else {
-        mensajeError.textContent = "La contrasena es inválida. Debe contener de 6 a 12 caracteres y puede incluir solo *, #, $ y caracteres alfanuméricos.";
-    }
-}
-function validarCorreo(){
-    var correo = document.querySelector('input[type="email').value
-    var mensaje = ''
-
-    if (correo.includes('@')){
-        var partes = correo.split('@')
-
-        if (partes.length > 2){
-            mensaje = 'No puede contener más de un @'
-        }
+        errorNick.textContent = "";
     }
 
-    if (!correo.includes('@')){
-        mensaje += 'No incluye @'
-    }
+    if (errores.length === 0) {
+        var usuario = {
+            nombre: nombre,
+            apellidos: apellidos,
+            contrasena: contrasena,
+            correo: correo,
+            nick: nick
+        };
 
-    if (correo.charAt(0) == '@'){
-        mensaje += '\nNo puede empezar por @'
-    }
 
-    for (let i = email.length - 3; i < email.length; i++){
-        if (email.charAt(i) == '@'){
-            mensaje += '\nNo puede tener @ en las tres últimas letras'
-        }
+        localStorage.setItem('usuario', JSON.stringify(usuario));
+
+        // Redirigir a la página de tu elección
+        window.location.href = "inicio.html";
     }
-    return mensaje;
 }
