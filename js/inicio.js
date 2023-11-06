fb5157ab4de344668dd34577b5a9f0d7658d8c8f
Marina <marinalaguna2004@gmail.com>
2023-11-06 10:44:58 +0100

    preparado el objeto para añadir imagenes

--- js/inicio.js
+++ js/inicio.js
@@ -13,29 +13,167 @@ function cargarCabecera2() {
 }
 
 const grandesPremios = [
-    { id: 1, lugar: 'SAKHIR', nombre: 'GP Bahréin', descripcion: 'El GP de Bahréin se celebra el 5 de Marzo en el circuito de Sakhir en Bahréin. La longitud del circuito es de 5,412 kilómetros. Durante la carrera se darán 57 vueltas al circuito cubriendo una distancia final de 308,484 kilómetros.' },
-    { id: 2, lugar: 'JEDDAH INTERNATIONAL STREET CIRCUIT', nombre: 'GP Arabia Saudí', descripcion: 'El GP de Arabia Saudí se celebra el 19 de Marzo en el circuito de Jeddah en Arabia Saudí. La longitud del circuito es de 6,174 kilómetros. Durante la carrera se darán 50 vueltas al circuito cubriendo una distancia final de 308,700 kilómetros.' },
-    { id: 3, lugar: 'ALBERT PARK', nombre: 'GP Australia', descripcion: 'El GP de Australia se celebra el 2 de Abril en el circuito de Melbourne en Australia. La longitud del circuito es de 5,303 kilómetros. Durante la carrera se darán 58 vueltas al circuito cubriendo una distancia final de 307,574 kilómetros.' },
-    { id: 4, lugar: 'BAKU CITY CIRCUIT', nombre: 'GP Azerbaiyán', descripcion: 'El GP de Azerbaiyán se celebra el 30 de Abril en el circuito de Bakú en Azerbaiyán. La longitud del circuito es de 6,003 kilómetros. Durante la carrera se darán 51 vueltas al circuito cubriendo una distancia final de 306,153 kilómetros.' },
-    { id: 5, lugar: 'HARD ROCK STADIUM CIRCUIT', nombre: 'GP Miami', descripcion: 'El GP de Miami se celebra el 7 de Mayo en el circuito de Miami International Autodrome en Miami (Estados Unidos). La longitud del circuito es de 5,412 kilómetros. Durante la carrera se darán 57 vueltas al circuito cubriendo una distancia final de 308,484 kilómetros.' },
-    { id: 6, lugar: 'IMOLA', nombre: 'GP Emilia Romagna', descripcion: 'El GP de Emilia-Romaña se celebra el 21 de Mayo en el circuito de Autodromo Enzo e Dino Ferrari en Imola (Italia). La longitud del circuito es de 4,909 kilómetros. Durante la carrera se darán 63 vueltas al circuito cubriendo una distancia final de 309,267 kilómetros.' },
-    { id: 7, lugar: 'MONTECARLO', nombre: 'GP Mónaco', descripcion: 'El GP de Mónaco se celebra el 28 de Mayo en el circuito de Montecarlo en Mónaco. La longitud del circuito es de 3,337 kilómetros. Durante la carrera se darán 78 vueltas al circuito cubriendo una distancia final de 260,286 kilómetros.' },
-    { id: 8, lugar: 'BARCELONA CATALUNYA', nombre: 'GP España', descripcion: 'El GP de España se celebra el 4 de Junio en el circuito de Barcelona-Cataluña (Montmeló) en Barcelona (España). La longitud del circuito es de 4,675 kilómetros. Durante la carrera se darán 66 vueltas al circuito cubriendo una distancia final de 308,550 kilómetros.' },
-    { id: 9, lugar: 'GILLES VILLENEUVE', nombre: 'GP Canadá', descripcion: 'El GP de Canadá se celebra el 18 de Junio en el circuito de Gilles Villeneuve en Montreal (Canadá). La longitud del circuito es de 4,361 kilómetros. Durante la carrera se darán 70 vueltas al circuito cubriendo una distancia final de 305,270 kilómetros.' },
-    { id: 10, lugar: 'RED BULL RING', nombre: 'GP Austria', descripcion: 'El GP de Austria se celebra el 2 de Julio en el circuito de Red Bull Ring en Spielberg (Austria). La longitud del circuito es de 4,318 kilómetros. Durante la carrera se darán 71 vueltas al circuito cubriendo una distancia final de 306,578 kilómetros.' },
-    { id: 11, lugar: 'SILVERSTONE', nombre: 'GP Gran Bretaña', descripcion: 'El GP de Gran Bretaña se celebra el 9 de Julio en el circuito de Silverstone en Reino Unido. La longitud del circuito es de 5,891 kilómetros. Durante la carrera se darán 52 vueltas al circuito cubriendo una distancia final de 306,332 kilómetros.' },
-    { id: 12, lugar: 'HUNGARORING', nombre: 'GP Hungría', descripcion: 'El GP de Hungría se celebra el 23 de Julio en el circuito de Hungaroring en Budapest (Hungría). La longitud del circuito es de 4,381 kilómetros. Durante la carrera se darán 70 vueltas al circuito cubriendo una distancia final de 306,670 kilómetros.' },
-    { id: 13, lugar: 'SPA-FRANCORCHAMPS', nombre: 'GP Bélgica', descripcion: 'El GP de Bélgica se celebra el 30 de Julio en el circuito de Spa-Francorchamps en Spa (Bélgica). La longitud del circuito es de 7,004 kilómetros. Durante la carrera se darán 44 vueltas al circuito cubriendo una distancia final de 308,176 kilómetros.' },
-    { id: 14, lugar: 'ZANDVOORT', nombre: 'GP Países Bajos', descripcion: 'El GP de los Países Bajos se celebra el 27 de Agosto en el circuito de Zandvoort en Países Bajos. La longitud del circuito es de 4,259 kilómetros. Durante la carrera se darán 72 vueltas al circuito cubriendo una distancia final de 306,648 kilómetros.' },
-    { id: 15, lugar: 'MONZA', nombre: 'GP Italia', descripcion: 'El GP de Italia se celebra el 3 de Septiembre en el circuito de Monza en Italia. La longitud del circuito es de 5,793 kilómetros. Durante la carrera se darán 53 vueltas al circuito cubriendo una distancia final de 307,029 kilómetros.' },
-    { id: 16, lugar: 'MARINA BAY', nombre: 'GP Singapur', descripcion: 'El GP de Singapur se celebra el 17 de Septiembre en el circuito de Marina Bay en Singapur (Singapur). La longitud del circuito es de 5,063 kilómetros. Durante la carrera se darán 61 vueltas al circuito cubriendo una distancia final de 308,843 kilómetros.' },
-    { id: 17, lugar: 'SUZUKA', nombre: 'GP Japón', descripcion: 'El GP de Japón se celebra el 24 de Septiembre en el circuito de Suzuka en Japón. La longitud del circuito es de 5,807 kilómetros. Durante la carrera se darán 53 vueltas al circuito cubriendo una distancia final de 307,771 kilómetros.' },
-    { id: 18, lugar: 'LOSAIL', nombre: 'GP Qatar', descripcion: 'El GP de Qatar se celebra el 8 de Octubre en el circuito de Lusail International Circuit en Lusail (Qatar). La longitud del circuito es de 5,38 kilómetros. Durante la carrera se darán 57 vueltas al circuito cubriendo una distancia final de 306,660 kilómetros.' },
-    { id: 19, lugar: 'LAS AMÉRICAS', nombre: 'GP Estados Unidos', descripcion: 'El GP de Estados Unidos se celebra el 22 de Octubre en el circuito de Las Américas en Austin (Texas) (Estados Unidos). La longitud del circuito es de 5,513 kilómetros. Durante la carrera se darán 56 vueltas al circuito cubriendo una distancia final de 308,728 kilómetros.' },
-    { id: 20, lugar: 'HERMANOS RODRÍGUEZ', nombre: 'GP México', descripcion: 'El GP de México se celebra el 29 de Octubre en el circuito de Los Hermanos Rodríguez en Ciudad de México (México). La longitud del circuito es de 4,304 kilómetros. Durante la carrera se darán 71 vueltas al circuito cubriendo una distancia final de 305,584 kilómetros.' },
-    { id: 21, lugar: 'INTERLAGOS', nombre: 'GP Sao Paulo', descripcion: 'El GP de Brasil se celebra el 5 de Noviembre en el circuito de José Carlos Pace en São Paulo (Brasil). La longitud del circuito es de 4,309 kilómetros. Durante la carrera se darán 71 vueltas al circuito cubriendo una distancia final de 305,939 kilómetros.' },
-    { id: 22, lugar: 'LAS VEGAS', nombre: 'LAS VEGAS', descripcion: 'El GP de Las Vegas se celebra el 18 de Noviembre en el circuito de Las Vegas en Estados Unidos. La longitud del circuito es de 6,12 kilómetros. Durante la carrera se darán 50 vueltas al circuito cubriendo una distancia final de 306,000 kilómetros.' },
-    { id: 23, lugar: 'YAS MARINA', nombre: 'GP Abu Dabi', descripcion: 'El GP de Abu Dabi se celebra el 26 de Noviembre en el circuito de Yas Marina en Abu Dabi (Emiratos Árabes). La longitud del circuito es de 5,281 kilómetros. Durante la carrera se darán 58 vueltas al circuito cubriendo una distancia final de 306,298 kilómetros.' }
+    { 
+        id: 1, 
+        lugar: 'Sakhir', 
+        nombre: 'GP Bahréin', 
+        descripcion: 'El GP de Bahréin se celebra el 5 de Marzo en el circuito de Sakhir en Bahréin. La longitud del circuito es de 5,412 kilómetros. Durante la carrera se darán 57 vueltas al circuito cubriendo una distancia final de 308,484 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 2, 
+        lugar: 'Jeddah', 
+        nombre: 'GP Arabia Saudí', 
+        descripcion: 'El GP de Arabia Saudí se celebra el 19 de Marzo en el circuito de Jeddah en Arabia Saudí. La longitud del circuito es de 6,174 kilómetros. Durante la carrera se darán 50 vueltas al circuito cubriendo una distancia final de 308,700 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 3, 
+        lugar: 'Melbourne', 
+        nombre: 'GP Australia', 
+        descripcion: 'El GP de Australia se celebra el 2 de Abril en el circuito de Melbourne en Australia. La longitud del circuito es de 5,303 kilómetros. Durante la carrera se darán 58 vueltas al circuito cubriendo una distancia final de 307,574 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 4, 
+        lugar: 'Baku', 
+        nombre: 'GP Azerbaiyán', 
+        descripcion: 'El GP de Azerbaiyán se celebra el 30 de Abril en el circuito de Bakú en Azerbaiyán. La longitud del circuito es de 6,003 kilómetros. Durante la carrera se darán 51 vueltas al circuito cubriendo una distancia final de 306,153 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 5, 
+        lugar: 'Miami', 
+        nombre: 'GP Miami', 
+        descripcion: 'El GP de Miami se celebra el 7 de Mayo en el circuito de Miami International Autodrome en Miami (Estados Unidos). La longitud del circuito es de 5,412 kilómetros. Durante la carrera se darán 57 vueltas al circuito cubriendo una distancia final de 308,484 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 6, 
+        lugar: 'Imola', 
+        nombre: 'GP Emilia Romagna', 
+        descripcion: 'El GP de Emilia-Romaña se celebra el 21 de Mayo en el circuito de Autodromo Enzo e Dino Ferrari en Imola (Italia). La longitud del circuito es de 4,909 kilómetros. Durante la carrera se darán 63 vueltas al circuito cubriendo una distancia final de 309,267 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 7, 
+        lugar: 'Montecarlo', 
+        nombre: 'GP Mónaco', 
+        descripcion: 'El GP de Mónaco se celebra el 28 de Mayo en el circuito de Montecarlo en Mónaco. La longitud del circuito es de 3,337 kilómetros. Durante la carrera se darán 78 vueltas al circuito cubriendo una distancia final de 260,286 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 8, 
+        lugar: 'Barcelona', 
+        nombre: 'GP España', 
+        descripcion: 'El GP de España se celebra el 4 de Junio en el circuito de Barcelona-Cataluña (Montmeló) en Barcelona (España). La longitud del circuito es de 4,675 kilómetros. Durante la carrera se darán 66 vueltas al circuito cubriendo una distancia final de 308,550 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 9, 
+        lugar: 'Montreal', 
+        nombre: 'GP Canadá', 
+        descripcion: 'El GP de Canadá se celebra el 18 de Junio en el circuito de Gilles Villeneuve en Montreal (Canadá). La longitud del circuito es de 4,361 kilómetros. Durante la carrera se darán 70 vueltas al circuito cubriendo una distancia final de 305,270 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 10, 
+        lugar: 'Spielberg', 
+        nombre: 'GP Austria', 
+        descripcion: 'El GP de Austria se celebra el 2 de Julio en el circuito de Red Bull Ring en Spielberg (Austria). La longitud del circuito es de 4,318 kilómetros. Durante la carrera se darán 71 vueltas al circuito cubriendo una distancia final de 306,578 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 11, 
+        lugar: 'Silverstone', 
+        nombre: 'GP Gran Bretaña', 
+        descripcion: 'El GP de Gran Bretaña se celebra el 9 de Julio en el circuito de Silverstone en Reino Unido. La longitud del circuito es de 5,891 kilómetros. Durante la carrera se darán 52 vueltas al circuito cubriendo una distancia final de 306,332 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 12, 
+        lugar: 'Budapest', 
+        nombre: 'GP Hungría', 
+        descripcion: 'El GP de Hungría se celebra el 23 de Julio en el circuito de Hungaroring en Budapest (Hungría). La longitud del circuito es de 4,381 kilómetros. Durante la carrera se darán 70 vueltas al circuito cubriendo una distancia final de 306,670 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 13, 
+        lugar: 'Spa', 
+        nombre: 'GP Bélgica', 
+        descripcion: 'El GP de Bélgica se celebra el 30 de Julio en el circuito de Spa-Francorchamps en Spa (Bélgica). La longitud del circuito es de 7,004 kilómetros. Durante la carrera se darán 44 vueltas al circuito cubriendo una distancia final de 308,176 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 14, 
+        lugar: 'Zandvoort', 
+        nombre: 'GP Países Bajos', 
+        descripcion: 'El GP de los Países Bajos se celebra el 27 de Agosto en el circuito de Zandvoort en Países Bajos. La longitud del circuito es de 4,259 kilómetros. Durante la carrera se darán 72 vueltas al circuito cubriendo una distancia final de 306,648 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 15, 
+        lugar: 'Monza', 
+        nombre: 'GP Italia', 
+        descripcion: 'El GP de Italia se celebra el 3 de Septiembre en el circuito de Monza en Italia. La longitud del circuito es de 5,793 kilómetros. Durante la carrera se darán 53 vueltas al circuito cubriendo una distancia final de 307,029 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 16, 
+        lugar: 'Singapur', 
+        nombre: 'GP Singapur', 
+        descripcion: 'El GP de Singapur se celebra el 17 de Septiembre en el circuito de Marina Bay en Singapur (Singapur). La longitud del circuito es de 5,063 kilómetros. Durante la carrera se darán 61 vueltas al circuito cubriendo una distancia final de 308,843 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 17, 
+        lugar: 'Suzuka', 
+        nombre: 'GP Japón', 
+        descripcion: 'El GP de Japón se celebra el 24 de Septiembre en el circuito de Suzuka en Japón. La longitud del circuito es de 5,807 kilómetros. Durante la carrera se darán 53 vueltas al circuito cubriendo una distancia final de 307,771 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 18, 
+        lugar: 'Lusail', 
+        nombre: 'GP Qatar', 
+        descripcion: 'El GP de Qatar se celebra el 8 de Octubre en el circuito de Lusail International Circuit en Lusail (Qatar). La longitud del circuito es de 5,38 kilómetros. Durante la carrera se darán 57 vueltas al circuito cubriendo una distancia final de 306,660 kilómetros.',
+        imagen: '' 
+    },
+    { 
+        id: 19, 
+        lugar: 'Austin (Texas)', 
+        nombre: 'GP Estados Unidos', 
+        descripcion: 'El GP de Estados Unidos se celebra el 22 de Octubre en el circuito de Las Américas en Austin (Texas) (Estados Unidos). La longitud del circuito es de 5,513 kilómetros. Durante la carrera se darán 56 vueltas al circuito cubriendo una distancia final de 308,728 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 20, 
+        lugar: 'Ciudad de México', 
+        nombre: 'GP México', 
+        descripcion: 'El GP de México se celebra el 29 de Octubre en el circuito de Los Hermanos Rodríguez en Ciudad de México (México). La longitud del circuito es de 4,304 kilómetros. Durante la carrera se darán 71 vueltas al circuito cubriendo una distancia final de 305,584 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 21, 
+        lugar: 'Sao Paulo', 
+        nombre: 'GP Sao Paulo', 
+        descripcion: 'El GP de Brasil se celebra el 5 de Noviembre en el circuito de José Carlos Pace en São Paulo (Brasil). La longitud del circuito es de 4,309 kilómetros. Durante la carrera se darán 71 vueltas al circuito cubriendo una distancia final de 305,939 kilómetros.',
+        imagen: ''
+    },
+    { 
+        id: 22, 
+        lugar: 'Las Vegas', 
+        nombre: 'LAS VEGAS', 
+        descripcion: 'El GP de Las Vegas se celebra el 18 de Noviembre en el circuito de Las Vegas en Estados Unidos. La longitud del circuito es de 6,12 kilómetros. Durante la carrera se darán 50 vueltas al circuito cubriendo una distancia final de 306,000 kilómetros.',
+        imagen: '' 
+    },
+    { 
+        id: 23, 
+        lugar: 'Abu Dabi', 
+        nombre: 'GP Abu Dabi', 
+        descripcion: 'El GP de Abu Dabi se celebra el 26 de Noviembre en el circuito de Yas Marina en Abu Dabi (Emiratos Árabes). La longitud del circuito es de 5,281 kilómetros. Durante la carrera se darán 58 vueltas al circuito cubriendo una distancia final de 306,298 kilómetros.',
+        imagen: '' 
+    }
 ];
 
 const usuariosBot = [
@@ -44,8 +182,8 @@ const usuariosBot = [
 ];
 
 const pilotos = [
-	{ id: 1, nombre: 'Alexander', apellidos: 'Albon', nacionalidad: '', propiedadJugador: 2, rol: 'Suplete' },
-    { id: 2, nombre: 'Carlos', apellidos: 'Sainz', nacionalidad: '', propiedadJugador: '', rol: 'Libre'},
+    { id: 1, nombre: 'Alexander', apellidos: 'Albon', nacionalidad: '', propiedadJugador: 2, rol: 'Suplete' },
+    { id: 2, nombre: 'Carlos', apellidos: 'Sainz', nacionalidad: '', propiedadJugador: '', rol: 'Libre' },
     { id: 3, nombre: 'Charles', apellidos: 'Leclerc', nacionalidad: '', propiedadJugador: '', rol: 'Titular' },
     { id: 4, nombre: 'Esteban', apellidos: 'Ocon', nacionalidad: '', propiedadJugador: '', rol: '' },
     { id: 5, nombre: 'Fernando', apellidos: 'Alonso', nacionalidad: '', propiedadJugador: '', rol: '' },
@@ -109,7 +247,7 @@ function asignarPilotos(usuario) {
     const pilotoTitularSeleccionado = prompt(`Pilotos disponibles: ${pilotosDisponibles.join(', ')}. Elige piloto titular:`);
     const pilotoSuplenteSeleccionado = prompt(`Pilotos disponibles: ${pilotosDisponibles.join(', ')}. Elige piloto suplente:`);
 
-    if (!pilotosDisponibles.includes(parseInt(pilotoTitularSeleccionado)) || !pilotosDisponibles.includes(parseInt(pilotoSuplenteSeleccionado))){
+    if (!pilotosDisponibles.includes(parseInt(pilotoTitularSeleccionado)) || !pilotosDisponibles.includes(parseInt(pilotoSuplenteSeleccionado))) {
         alert("Los pilotos seleccionados no están disponibles.");
         return;
     }
