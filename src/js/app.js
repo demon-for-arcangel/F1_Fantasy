108a1b5e3f4a505e400dbdf62a0ad58acfedd573
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2023-11-08 23:50:46 +0100

    correcion del objeto usuario

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
+import { usuario } from "./objetos.js";
+
+
 function cargarCabecera() {
     document.getElementById("cabecera").innerHTML = 
     `
@@ -37,59 +40,13 @@ function cargarPiePagina(){
 }
 
 function btnInicioSesion(){
-    window.location.href = "../index.html"
+    window.location.href = "../html/index.html"
 }
 
 function btnRegistrar(){
-    window.location.href = "../registro.html"
+    window.location.href = "../html/registro.html"
 }
 
 function btnSignIn(){
-    window.location.href = "../inicio.html"
-}
-
-function validarInicio(){
-    var correo = document.querySelector('input[type="email]').value
-    var contrasena = document.querySelector('input[type = "password"]').value
-    var mensaje = validarCorreo()
-
-    if (mensaje === ''){
-        if (correo.trim() == '' || contrasena.trim() == ''){
-            mensaje += 'El correo y la contraseña no pueden estar vacíos.'
-            return mensaje
-        }
-        if (contrasena.length < 8){
-            mensaje += 'La contraseña debe ser de 8 caracteres como mínimo.'
-            return mensaje
-        }
-    }
-    return mensaje
-}
-
-function validarCorreo(){
-    var correo = document.querySelector('input[type="email').value
-    var mensaje = ''
-
-    if (correo.includes('@')){
-        var partes = correo.split('@')
-
-        if (partes.length > 2){
-            mensaje = 'No puede contener más de un @'
-        }
-    }
-
-    if (!correo.includes('@')){
-        mensaje += 'No incluye @'
-    }
-
-    if (correo.charAt(0) == '@'){
-        mensaje += '\nNo puede empezar por @'
-    }
-
-    for (let i = email.length - 3; i < email.length; i++){
-        if (email.charAt(i) == '@'){
-            mensaje += '\nNo puede tener @ en las tres últimas letras'
-        }
-    }
-    return mensaje;
+    window.location.href = "../html/inicio.html"
 }
