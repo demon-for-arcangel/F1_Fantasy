79573d5859eee2631f389f1e9fd918ae1ebc1c09
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2023-11-22 22:32:06 +0100

    update

--- src/js/cargarElementos.js
+++ src/js/cargarElementos.js
@@ -12,10 +12,10 @@ function cargarCabecera2() {
         `
         <header class = "cabeceraOrientacion">
             <a href="inicio.html" class = "logo"><img src="../imagenes/f1_logo.svg"></a>
-            <a href="clasificación.html"><p>Clasificación</p></a>
+            <a href="clasificacion.html"><p>Clasificación</p></a>
             <a href="tuspilotos.html"><p>Tus pilotos</p></a>
             <a href="pilotos.html"><p>Pilotos</p></a>
-            <a href="administración.html"><p>Administración</p></a>
+            <a href="administracion.html"><p>Administración</p></a>
             <a href="perfil.html" class = "iconoPerfil"><img src = "../imagenes/iconos/icono_perfil.png"></a>
             <button id = "btnOrientacion" onclick = "cambiarOrientacion()">Cambiar Orientación</button>
         </header>
