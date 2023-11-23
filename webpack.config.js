dee21f17ebce8d052504dd5a5509315e0944536d
Marina Laguna <marinalaguna2004@gmail.com>
2023-11-23 14:09:18 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/app.js',
@@ -6,4 +7,46 @@ module.exports = {
         filename: 'bundle,js',
         path: path.resolve(__dirname, 'dist'),
     },
+
+    plugins:[
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+
+      new HtmlWebpackPlugin({
+        template: './src/registro.html',
+        filename: 'registro.html',
+      }),
+
+      new HtmlWebpackPlugin({
+        template: './src/inicio.html',
+        filename: 'inicio.html',
+      }),
+
+      new HtmlWebpackPlugin({
+        template: './src/clasificacion.html',
+        filename: 'clasificacion.html',
+      }),
+
+      new HtmlWebpackPlugin({
+        template: './src/tusPilotos.html',
+        filename: 'tusPilotos.html',
+      }),
+
+      new HtmlWebpackPlugin({
+        template: './src/pilotos.html',
+        filename: 'pilotos.html',
+      }),
+
+      new HtmlWebpackPlugin({
+        template: './src/administracion.html',
+        filename: 'administracion.html',
+      }),
+
+      new HtmlWebpackPlugin({
+        template: './src/perfil.html',
+        filename: 'perfil.html',
+      }),
+    ]
 };
